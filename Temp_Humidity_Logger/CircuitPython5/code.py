9c06ca3c7229070a6b05ff0c88a93bd728db9d8a
Colin Grant <colindgrant@gmail.com>
2020-03-07 12:19:15 -0800

    Initial experimentation on temperature and humidity logger.
    Code is used for evaluating the following hardware:
    Adafruit Feather M4
    Adafruit RTC + SD Featherwing
    Adafruit HTU21D-F
    Adafruit SHT31-D
    
    CircuitPython5 directory contains the lib/ folder and code.py that runs when dropped onto the Feather's CIRCUITPY USB volume.
    Current functionality:
    - Reads temp and humidity from SHT and HTU sensors
    - Averages temp values
    - Writes time, temp, humidity to SD card
    
    The .ino file is an Adruino sketch combining two Adafruit PCF8523 [RTC] and CardInfo [SD] examples.
    Current functionality:
    - Sets time on RTC to compile time
    - Reads and prints time
    - Reads and prints basic SD card info

---
+++ Temp_Humidity_Logger/CircuitPython5/code.py
@@ -0,0 +1,139 @@
+import time
+import board
+import busio
+import digitalio
+from analogio import AnalogIn
+import os
+import adafruit_sdcard
+import storage
+
+import adafruit_ds1307
+import adafruit_pcf8523
+
+from adafruit_htu21d import HTU21D
+import adafruit_sht31d
+
+# Get battery and system voltage
+vbat_voltage = AnalogIn(board.VOLTAGE_MONITOR)
+dvdr_voltage = AnalogIn(board.A5) # voltage divider with 2 equal resistors (220 Ohm)
+
+# Create library object using our Bus I2C port
+i2c_bus = busio.I2C(board.SCL, board.SDA)
+
+# Initialize peripherals
+htu = HTU21D(i2c_bus)
+sht = adafruit_sht31d.SHT31D(i2c_bus)
+
+# rtc = adafruit_ds1307.DS1307(i2c_bus)
+rtc = adafruit_pcf8523.PCF8523(i2c_bus)
+
+# Set up SPI for the SD card logger
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+cs = digitalio.DigitalInOut(board.D10)
+sdcard = adafruit_sdcard.SDCard(spi, cs)
+vfs = storage.VfsFat(sdcard)
+storage.mount(vfs, "/sd")
+sd_test_file = "/sd/testfile.txt"
+
+# Overwrite existing file, effectively wiping old data
+with open(sd_test_file, "w") as f:
+    f.write("Hello world!\r\n")
+
+# Lookup table for names of days (nicer printing).
+days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+
+# Configure averaging for sensor readings
+NUM_TEMP_SAMPLES = 5
+TIME_BET_SAMPLES = 1
+
+def print_directory(path, tabs=0):
+    for file in os.listdir(path):
+        stats = os.stat(path + "/" + file)
+        filesize = stats[6]
+        isdir = stats[0] & 0x4000
+
+        if filesize < 1000:
+            sizestr = str(filesize) + " by"
+        elif filesize < 1000000:
+            sizestr = "%0.1f KB" % (filesize / 1000)
+        else:
+            sizestr = "%0.1f MB" % (filesize / 1000000)
+
+        prettyprintname = ""
+        for _ in range(tabs):
+            prettyprintname += "   "
+        prettyprintname += file
+        if isdir:
+            prettyprintname += "/"
+        print('{0:<40} Size: {1:>10}'.format(prettyprintname, sizestr))
+
+        # recursively print directory contents
+        if isdir:
+            print_directory(path + "/" + file, tabs + 1)
+
+def get_voltage(pin):
+    return (pin.value * 3.3) / 65536 * 2 #actaul Aref is more like 3.286
+
+if False:   # change to True if you want to set the time!
+    #                     year, mon, date, hour, min, sec, wday, yday, isdst
+    t = time.struct_time((2020,  02,   25,   16,  12,  30,    2,   -1,    -1))
+    # you must set year, mon, date, hour, min, sec and weekday
+    # yearday is not supported, isdst can be set but we don't do anything with it at this time
+    print("Setting time to:", t)     # uncomment for debugging
+    rtc.datetime = t
+
+while True:
+
+#     print("\nFiles on filesystem:")
+#     print("====================")
+#     print_directory("/sd")
+
+    print("\nContents of {}:".format(sd_test_file))
+    print("====================")
+
+    # read and print one line at a time, no memory risk
+    with open(sd_test_file, "r") as f:
+        line = f.readline()
+        while line != '':
+            print(line, end='')
+            line = f.readline()
+
+    # read whole file into memory, could overflow
+#     with open(sd_test_file, "r") as f:
+#         lines = f.readlines()
+#         for line in lines:
+#             print(line, end='')
+
+    # averaged temps
+    htu_cumm_samples = 0
+    sht_cumm_samples = 0
+    for i in range(NUM_TEMP_SAMPLES):
+        htu_cumm_samples += htu.temperature
+        sht_cumm_samples += sht.temperature
+        time.sleep(TIME_BET_SAMPLES)
+
+    htu_temperature_c = htu_cumm_samples / NUM_TEMP_SAMPLES
+    sht_temperature_c = sht_cumm_samples / NUM_TEMP_SAMPLES
+
+    time_date = rtc.datetime
+
+    print("The date is: {}, {}-{:02}-{:02}".format(days[int(time_date.tm_wday)], time_date.tm_year, time_date.tm_mon, time_date.tm_mday))
+    print("The time is: {}:{:02}:{:02}".format(time_date.tm_hour, time_date.tm_min, time_date.tm_sec))
+    print("Epoch    is: {}".format(time.mktime(time_date)))
+
+    print("\nThe VReg is: {:.2f}".format(get_voltage(dvdr_voltage)))
+    print("The VBat is: {:.2f}".format(get_voltage(vbat_voltage)))
+
+    print("\nHTU21D Temperature: {:0.3f} C".format(htu_temperature_c))
+    print("HTU21D Humidity: {:0.1f} %".format(htu.relative_humidity))
+
+    print("\nSHT31D Temperature: {:0.3f} C".format(sht_temperature_c))
+    print("SHT31D Humidity: {:0.1f} %".format(sht.relative_humidity))
+    print("\n")
+
+    # Write sample string, e.g.: 1583575730,19.489,48.5
+    with open(sd_test_file, "a") as f:
+        f.write( "{},{:0.3f},{:0.1f}\r\n".format(time.mktime(time_date), htu_temperature_c, htu.relative_humidity) )
+
+    # Plotter color order: Green, Blue, Orange, Grey
+    print((htu_temperature_c, sht_temperature_c, htu_temperature_c - sht_temperature_c))
---
+++ Temp_Humidity_Logger/Temp_Humidity_Logger.ino
@@ -0,0 +1,196 @@
+/*
+  Colin 2020-03-07
+  Basic validation of Feather M4 with RTC + SD Featherwing
+  This sketch combines Adafruit PCF8523 and CardInfo examples into one:
+  
+
+  Date and time functions using a PCF8523 RTC connected via I2C and Wire lib
+
+  
+  SD card test
+
+  This example shows how use the utility libraries on which the'
+  SD library is based in order to get info about your SD card.
+  Very useful for testing a card when you're not sure whether its working or not.
+
+  The circuit:
+    SD card attached to SPI bus as follows:
+ ** MOSI - pin 11 on Arduino Uno/Duemilanove/Diecimila
+ ** MISO - pin 12 on Arduino Uno/Duemilanove/Diecimila
+ ** CLK - pin 13 on Arduino Uno/Duemilanove/Diecimila
+ ** CS - depends on your SD card shield or module.
+     Pin 4 used here for consistency with other Arduino examples
+
+
+  created  28 Mar 2011
+  by Limor Fried
+  modified 9 Apr 2012
+  by Tom Igoe
+*/
+
+
+// include general RTC library that works with PCF8523
+#include "RTClib.h"
+
+// include the SD library:
+#include <SPI.h>
+#include <SD.h>
+
+// Set up RTC variables
+RTC_PCF8523 rtc;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+// set up variables using the SD utility library functions:
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+
+// change this to match your SD shield or module;
+// Arduino Ethernet shield: pin 4
+// Adafruit SD shields and modules: pin 10
+// Sparkfun SD shield: pin 8
+// MKRZero SD: SDCARD_SS_PIN
+const int chipSelect = 10;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(57600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  Serial.println("Starting RTC...");
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  Serial.println("Setting current time...");
+  if (! rtc.initialized()) {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+
+  for (byte i=0; i < 5; i++) {
+    showtime();
+    delay(1000);
+  }
+
+  Serial.print("\nInitializing SD card...");
+
+  // we'll use the initialization code from the utility libraries
+  // since we're just testing if the card is working!
+  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card inserted?");
+    Serial.println("* is your wiring correct?");
+    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+    while (1);
+  } else {
+    Serial.println("Wiring is correct and a card is present.");
+  }
+
+  // print the type of card
+  Serial.println();
+  Serial.print("Card type:         ");
+  switch (card.type()) {
+    case SD_CARD_TYPE_SD1:
+      Serial.println("SD1");
+      break;
+    case SD_CARD_TYPE_SD2:
+      Serial.println("SD2");
+      break;
+    case SD_CARD_TYPE_SDHC:
+      Serial.println("SDHC");
+      break;
+    default:
+      Serial.println("Unknown");
+  }
+
+  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+  if (!volume.init(card)) {
+    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    while (1);
+  }
+
+  Serial.print("Clusters:          ");
+  Serial.println(volume.clusterCount());
+  Serial.print("Blocks x Cluster:  ");
+  Serial.println(volume.blocksPerCluster());
+
+  Serial.print("Total Blocks:      ");
+  Serial.println(volume.blocksPerCluster() * volume.clusterCount());
+  Serial.println();
+
+  // print the type and size of the first FAT-type volume
+  uint32_t volumesize;
+  Serial.print("Volume type is:    FAT");
+  Serial.println(volume.fatType(), DEC);
+
+  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
+  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
+  volumesize /= 2;                           // SD card blocks are always 512 bytes (2 blocks are 1KB)
+  Serial.print("Volume size (Kb):  ");
+  Serial.println(volumesize);
+  Serial.print("Volume size (Mb):  ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+  Serial.print("Volume size (Gb):  ");
+  Serial.println((float)volumesize / 1024.0);
+
+  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
+  root.openRoot(volume);
+
+  // list all files in the card with date and size
+  root.ls(LS_R | LS_DATE | LS_SIZE);
+}
+
+void showtime() {
+    DateTime now = rtc.now();
+
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+    Serial.print(" since midnight 1/1/1970 = ");
+    Serial.print(now.unixtime());
+    Serial.print("s = ");
+    Serial.print(now.unixtime() / 86400L);
+    Serial.println("d");
+
+    // calculate a date which is 7 days, 12 hours and 30 seconds into the future
+    DateTime future (now + TimeSpan(7,12,30,6));
+
+    Serial.print(" now + 7d + 12h + 30m + 6s: ");
+    Serial.print(future.year(), DEC);
+    Serial.print('/');
+    Serial.print(future.month(), DEC);
+    Serial.print('/');
+    Serial.print(future.day(), DEC);
+    Serial.print(' ');
+    Serial.print(future.hour(), DEC);
+    Serial.print(':');
+    Serial.print(future.minute(), DEC);
+    Serial.print(':');
+    Serial.print(future.second(), DEC);
+    Serial.println();
+    Serial.println();
+}
+
+void loop(void) {
+}
