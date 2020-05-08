17367a8bc414a8dab5aac81db2c0bb78c69bf5f5
Colin Grant <colindgrant@gmail.com>
2020-05-07 18:04:22 -0700

    Basic Bluetooth support: data string over bleuart
    Now the sample data string is written three places every 30 seconds:
    - SD card
    - Serial Monitor
    - BLE UART monitor in Adafruit Bluefruit Connect app
    
    BLE setup code from Adafruit's Bluefruit nRF52840 blueart example.
    
    This also prints the RTC time to bleuart so I can compare with iPhone time to determine if RTC is off.

--- TempHumidityLogger/TempHumidityLogger.ino
+++ TempHumidityLogger/TempHumidityLogger.ino
@@ -18,6 +18,17 @@
 #include <SPI.h>             // SD
 #include <SdFat.h>           // Filesystem
 
+#include <bluefruit.h>
+#include <Adafruit_LittleFS.h>
+#include <InternalFileSystem.h>
+
+//#define CFG_DEBUG true
+
+// BLE Service
+BLEDfu  bledfu;  // OTA DFU service
+BLEDis  bledis;  // device information
+BLEUart bleuart; // uart over ble
+BLEBas  blebas;  // battery
 
 ////////////////////////////////////////////////////////////////////////////////
 // Dev selections for switching between board types, etc.
@@ -173,9 +184,125 @@ void setup() {
   MYSERIAL.print(F("Logging to: "));
   MYSERIAL.println(fileName);
 
+  // Setup the BLE LED to be enabled on CONNECT
+  // Note: This is actually the default behaviour, but provided
+  // here in case you want to control this LED manually via PIN 19
+  Bluefruit.autoConnLed(true);
+
+  // Config the peripheral connection with maximum bandwidth 
+  // more SRAM required by SoftDevice
+  // Note: All config***() function must be called before begin()
+  Bluefruit.configPrphBandwidth(BANDWIDTH_MAX);
+
+  Bluefruit.begin();
+  Bluefruit.setTxPower(4);    // Check bluefruit.h for supported values
+  Bluefruit.setName("Bluefruit52");
+  //Bluefruit.setName(getMcuUniqueID()); // useful testing with multiple central connections
+  Bluefruit.Periph.setConnectCallback(connect_callback);
+  Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
+
+  // To be consistent OTA DFU should be added first if it exists
+  bledfu.begin();
+
+  // Configure and Start Device Information Service
+  bledis.setManufacturer("Adafruit Industries");
+  bledis.setModel("Bluefruit Feather52");
+  bledis.begin();
+
+  // Configure and Start BLE Uart Service
+  bleuart.begin();
+
+  // Start BLE Battery Service
+  blebas.begin();
+  blebas.write(100);
+
+  // Set up and start advertising
+  startAdv();
+
+  MYSERIAL.println("Please use Adafruit's Bluefruit LE app to connect in UART mode");
+  MYSERIAL.println("Once connected, enter character(s) that you wish to send");
+
   // We're done with setup stuff, now start the interrupt loop.
   enableRTC();
 }
+
+void startAdv(void)
+{
+  // Advertising packet
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+
+  // Include bleuart 128-bit uuid
+  Bluefruit.Advertising.addService(bleuart);
+
+  // Secondary Scan Response packet (optional)
+  // Since there is no room for 'Name' in Advertising packet
+  Bluefruit.ScanResponse.addName();
+  
+  /* Start Advertising
+   * - Enable auto advertising if disconnected
+   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+   * - Timeout for fast mode is 30 seconds
+   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
+   * 
+   * For recommended advertising interval
+   * https://developer.apple.com/library/content/qa/qa1931/_index.html   
+   */
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
+  Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
+  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds  
+}
+
+// callback invoked when central connects
+void connect_callback(uint16_t conn_handle)
+{
+  // Get the reference to current connection
+  BLEConnection* connection = Bluefruit.Connection(conn_handle);
+
+  char central_name[32] = { 0 };
+  connection->getPeerName(central_name, sizeof(central_name));
+
+  MYSERIAL.print("Connected to ");
+  MYSERIAL.println(central_name);
+}
+
+/**
+ * Callback invoked when a connection is dropped
+ * @param conn_handle connection where this event happens
+ * @param reason is a BLE_HCI_STATUS_CODE which can be found in ble_hci.h
+ */
+void disconnect_callback(uint16_t conn_handle, uint8_t reason)
+{
+  (void) conn_handle;
+  (void) reason;
+
+  MYSERIAL.println();
+  MYSERIAL.print("Disconnected, reason = 0x"); MYSERIAL.println(reason, HEX);
+}
+
+void fowardBleuart() {
+  // Forward data from HW Serial to BLEUART
+  while (MYSERIAL.available())
+  {
+    // Delay to wait for enough input, since we have a limited transmission buffer
+    delay(2);
+
+    uint8_t buf[64];
+    int count = MYSERIAL.readBytes(buf, sizeof(buf));
+    bleuart.write( buf, count );
+  }
+
+  // Forward from BLEUART to HW Serial
+  while ( bleuart.available() )
+  {
+    uint8_t ch;
+    ch = (uint8_t) bleuart.read();
+    MYSERIAL.write(ch);
+  }
+}
+
+
 
 void enableRTC() {
   // Take the first sample at beginning of the minute
@@ -218,7 +345,7 @@ void printStoredSamples() {
 }
 
 void showTime(DateTime now) {
-  MYSERIAL.print(F("The current time is: "));
+  MYSERIAL.print(F("RTC time is: "));
   MYSERIAL.print(now.year(), DEC);
   MYSERIAL.print('/');
   MYSERIAL.print(now.month(), DEC);
@@ -264,11 +391,11 @@ void takeSample() {
   // Char arrary for the CSV data string to be be stored on SD card
   // Should only need 50 chars, but altitude can be negative, etc.
   char csv[64];
-  sprintf(csv, "%lu,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f\n", now.unixtime(), voltage,
+  sprintf(csv, "%lu,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f", now.unixtime(), voltage,
           relh_sht31, temp_sht31, tmp_bmp280, bar_bmp280, alt_bmp280);
 
   // Write data to SD file, note string already ends with a new line
-  file.print(csv);
+  file.println(csv);
 
 #ifdef DEBUG
   // Pretty print individual labeled measures
@@ -284,6 +411,12 @@ void takeSample() {
 
   // Write data string to Serial
   MYSERIAL.println(csv);
+
+  // Write RTC and data string to Bleuart
+  char timeStr[16];
+  sprintf(timeStr, "RTC: %02hd:%02hd:%02hd", now.hour(), now.minute(), now.second());
+  bleuart.print(timeStr);  
+  bleuart.print(csv);
 
   // Force data to SD and update the directory entry to avoid data loss.
   if (!file.sync() || file.getWriteError()) {
@@ -318,4 +451,7 @@ void loop(void) {
     printStoredSamples();
 #endif
   }
+
+  fowardBleuart();
+  
 }
